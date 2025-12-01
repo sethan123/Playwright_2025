fc110bbeeb2656dda160f1af68f6d13c13cda035
sethan <sethan15@hotmail.com>
2025-12-01 12:16:05 +0530

    Updated

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,91 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('@Webst Client App login', async ({ page }) => {
+   //js file- Login js, DashboardPage
+   const email = "sethan15@hotmail.com";
+   const productName = 'ZARA COAT 3';
+   const products = page.locator(".card-body");
+   await page.goto("https://rahulshettyacademy.com/client");
+   await page.locator("#userEmail").fill(email);
+   await page.locator("#userPassword").fill("Chennai@143");
+   await page.locator("[value='Login']").click();
+   await page.waitForLoadState('networkidle');
+   await page.locator(".card-body b").first().waitFor();
+   const titles = await page.locator(".card-body b").allTextContents();
+   console.log(titles); 
+   const count = await products.count();
+   for (let i = 0; i < count; ++i) {
+      if (await products.nth(i).locator("b").textContent() === productName) {
+         //add to cart
+         await products.nth(i).locator("text= Add To Cart").click();
+         break;
+      }
+   }
+ 
+   await page.locator("[routerlink*='cart']").click();
+   //await page.pause();
+ 
+   await page.locator("div li").first().waitFor();
+   const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+   expect(bool).toBeTruthy();
+   await page.locator("text=Checkout").click();
+ 
+  await page.getByPlaceholder('Select Country').pressSequentially("ind", { delay: 150 }) 
+   const dropdown = page.locator(".ta-results");
+   await dropdown.waitFor();
+   const optionsCount = await dropdown.locator("button").count();
+   for (let i = 0; i < optionsCount; ++i) {
+      const text = await dropdown.locator("button").nth(i).textContent();
+      if (text === " India") {
+         await dropdown.locator("button").nth(i).click();
+         break;
+      }
+   }
+ 
+   expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   console.log(orderId);
+ 
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+ 
+ 
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      //if (orderId.includes(rowOrderId)) {
+       if (orderId && rowOrderId && orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+  // const orderIdDetails = await page.locator(".col-text").textContent();
+   //expect(orderId.includes(orderIdDetails)).toBeTruthy();
+ const orderIdDetails = (await page.locator(".col-text").textContent())?.trim() ?? "";
++  expect(orderId).toContain(orderIdDetails);
+});
+ 
+
+test('Playwright Special locators', async ({ page }) => {
+  
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").check();
+    await page.getByLabel("Gender").selectOption("Female");
+    await page.getByPlaceholder("Password").fill("abc123");
+    await page.getByRole("button", {name: 'Submit'}).click();
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link",{name : "Shop"}).click();
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
+ 
+    //locator(css)
+ 
+});
+ 
+ 
+ 
